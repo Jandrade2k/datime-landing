5283754d99c64252ea1aa0e96eecf6c85a639cf3
Jandrade2k <dev.jandrade@gmail.com>
2022-03-29 16:32:55 -0300

    commit backup

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { FaLinkedin, FaWhatsapp, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 import './style.css'
 
 export function Contact() {
     return (
         <div className="contact">
             <div>
-                
+                <FaPhoneAlt color="#fff" size={30} />
+                <p>(xx) xxxx - xxx</p>
+            </div>
+            <div>
+                <FaWhatsapp color="#fff" size={30} />
+                <p>(xx) xxxxx - xxx</p>
+            </div>
+            <div>
+                <FaEnvelope color="#fff" size={30} />
+                <p>example@datime.com.br</p>
+            </div>
+            <div>
+                <FaLinkedin color="#fff" size={30} />
+                <p>/datime</p>
             </div>
         </div>
     )
 }
+
+
+// pega no meu paau
